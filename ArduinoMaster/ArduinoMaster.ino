1a955c362b7360bb2cbdc2919a0ed8d60fb95f5b
Raphael Schachenmayr-Schlick <deepspace84@outlook.com>
2021-07-03 23:53:58 +0200

    Add project files.

---
+++ ArduinoMaster/ArduinoMaster.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ArduinoMaster.ino
+ Created:	7/3/2021 11:52:55 PM
+ Author:	Deepspace
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
